86c31871de085480b812bbcc999890fdba8bc4bd
hahosameer <ks4063180@gmail.com>
2024-06-17 15:43:25 +0500

    new changes

--- index.js
+++ index.js
@@ -1,102 +1,3 @@
-// import express from "express";
-// import cors from "cors"; // Import cors middleware
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// import helmet from "helmet";
-// import morgan from "morgan";
-// import { authRoute } from "./routes/auth.js";
-// import { userRoute } from "./routes/users.js";
-// import { postRoute } from "./routes/posts.js";
-// import multer from "multer";
-// import path from "path";
-// import { cloudinary } from "cloudinary";
-// dotenv.config();
-
-
-// // Cloudinary configuration
-// cloudinary.v2.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
-
-
-
-
-
-// const app = express();
-
-// // Database connection
-// const DBConnection = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI);
-//     console.log("DB Connected");
-//   } catch (err) {
-//     console.error("Database connection error:", err);
-//     process.exit(1);
-//   }
-// };
-
-// DBConnection();
-
-// // Static file serving
-// const __dirname = path.resolve();
-// app.use("/images", express.static(path.join(__dirname, "public/images")));
-
-// // Middleware
-// app.use(express.json());
-// app.use(helmet());
-// app.use(morgan("common"));
-
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173", "https://facebook-frontend.vercel.app"],
-//   })
-// );
-// app.options("*", cors()); // Allow pre-flight requests
-
-// // File upload configuration
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "public/images");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, req.body.name);
-//   },
-// });
-// const upload = multer({ storage: storage });
-
-// // File upload endpoint
-// app.post("/api/upload", upload.single("file"), (req, res) => {
-//   try {
-//     return res.status(200).json("File uploaded successfully");
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).json({ error: "File upload failed" });
-//   }
-// });
-
-// // Routes
-// app.use("/api/auth", authRoute);
-// app.use("/api/posts", postRoute);
-// app.use("/api/users", userRoute);
-
-// // Error handling middleware
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send("Something went wrong!");
-// });
-
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
-// // Server listening
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Backend Server is running on port ${PORT}`);
-// });
-
 
 
 import express from "express";
@@ -143,7 +44,6 @@ app.use(morgan("common"));
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://facebook-frontend.vercel.app"],
-    credentials: true,
   })
 );
 
@@ -152,7 +52,7 @@ app.use(
 //     origin: "https://facebook-frontend.vercel.app/"
 //   })
 // );
-// app.options("*", cors()); // Enable pre-flight requests for all routes
+app.options("*", cors()); // Enable pre-flight requests for all routes
 
 // Cloudinary configuration
 cloudinary.config({
@@ -195,9 +95,9 @@ app.post("/api/upload", upload.single("file"), async (req, res) => {
     console.log(result.url, "resultresultresultresult");
 
     // Delete the file from local storage after uploading to Cloudinary
-    fs.unlink(filePath, (err) => {
-      if (err) console.error("Failed to delete local file:", err);
-    });
+    // fs.unlink(filePath, (err) => {
+    //   if (err) console.error("Failed to delete local file:", err);
+    // });
     return res
       .status(200)
       .json({ message: "File uploaded successfully", url: result.secure_url });
@@ -230,3 +130,21 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Backend Server is running on port ${PORT}`);
 });
+
+
+
+// {
+//     "version": 2,
+//     "builds": [
+//       {
+//         "src": "./index.js",
+//         "use": "@vercel/node"
+//       }
+//     ],
+//     "routes": [
+//       {
+//         "src": "/(.*)",
+//         "dest": "/"
+//       }
+//     ]
+//   }
